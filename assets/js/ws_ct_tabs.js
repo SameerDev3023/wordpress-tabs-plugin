5c39d19687692111c9d5f74776387d35714d3fe0
sameer <sameerdeveloper05@gmail.com>
2023-06-18 22:10:50 +0530

    customize some js code

--- assets/js/ws_ct_tabs.js
+++ assets/js/ws_ct_tabs.js
@@ -1,25 +1,19 @@
-let TabsBtn = document.getElementsByClassName('nav-link');
 let TabsContent = document.getElementsByClassName('tab-pane');
-
+let TabsBtn = document.getElementsByClassName('nav-link');
 let GetAllTabsElm = Array.from(TabsBtn);
 let TabsContent2 = Array.from(TabsContent);
-
-// Set the first tab and content as active initially
-TabsBtn[0].classList.add('active');
-TabsContent[0].classList.add('active');
-
 GetAllTabsElm.forEach((tabsBtn, index) => {
-    tabsBtn.addEventListener('click', function() {
-        // Remove 'active' class from all tabs and contents
-        GetAllTabsElm.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-        TabsContent2.forEach((contentElm) => {
-            contentElm.classList.remove('active');
-        });
-
-        // Add 'active' class to the clicked tab and its corresponding content
+    tabsBtn.addEventListener('click', function() {     
+        let parentElem = this.closest('.card');
+        let TabsContent3 = parentElem.getElementsByClassName('tab-pane');
+        let TabsBtn4 = parentElem.getElementsByClassName('nav-link');
+        for(val of TabsContent3 ){
+         val.classList.remove('active')
+        }
+        for(val of TabsBtn4 ){
+            val.classList.remove('active')
+         }
         this.classList.add('active');
-        TabsContent[index].classList.add('active');
+        TabsContent2[index].classList.add('active');
     });
 });
